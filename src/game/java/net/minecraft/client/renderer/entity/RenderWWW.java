81711b453303f9d7791260a10171add14c27c22e
Mutant Bloodcell <mutantbloodcell@gmail.com>
2026-08-26 03:27:22 +1200

    Create RenderWWW.java

---
+++ src/game/java/net/minecraft/client/renderer/entity/RenderWWW.java
@@ -0,0 +1,44 @@
+package net.minecraft.client.renderer.entity;
+
+import net.minecraft.client.Minecraft;
+import net.minecraft.client.model.ModelPlayer;
+import net.minecraft.client.renderer.GlStateManager;
+import net.minecraft.entity.monster.EntityWWW;
+import net.minecraft.entity.player.EntityPlayer;
+import net.minecraft.util.ResourceLocation;
+
+public class RenderStalker extends RenderLiving<EntityWWW> {
+    
+    private static final ResourceLocation stalkerTexture = new ResourceLocation("textures/entity/steve.png");
+
+    public RenderStalker(RenderManager renderManagerIn) {
+        
+        super(renderManagerIn, new ModelPlayer(0.0F, false), 0.5F);
+    }
+
+    @Override
+    protected ResourceLocation getEntityTexture(EntityWWW entity) {
+        return stalkerTexture;
+    }
+
+    @Override
+    protected void preRenderCallback(EntityWWW entitylivingbaseIn, float partialTickTime) {
+        EntityPlayer player = Minecraft.getMinecraft().thePlayer;
+        float alpha = 1.0F;
+
+        if (player != null) {
+            double distance = entitylivingbaseIn.getDistanceToEntity(player);
+            
+            if (distance < 30.0D) {
+                alpha = (float)((distance - 15.0D) / 15.0D);
+            }
+        }
+
+        
+        GlStateManager.enableBlend();
+        GlStateManager.blendFunc(770, 771);
+
+        
+        GlStateManager.color(0.0F, 0.0F, 0.0F, alpha);
+    }
+}
